21b347f461ca23a6ec64399f6763e43f46ecdf48
Federico Almonacid <fede.cur.4.agosto@gmail.com>
2025-01-12 03:43:24 -0300

    Erroes corregidos en templates y mejora de detalles

--- src/Chicago/views.py
+++ src/Chicago/views.py
@@ -4,33 +4,37 @@ from .models import Producto, Cliente, Venta
 
 from .forms import VentasForm, ProductoForm, ClientesForm
 
-def index(request):
+from django.http import HttpResponse, HttpRequest
+
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "Chicago/index.html")
 
 def about(request):
     return render(request, "Chicago/about.html")
 
-def Clientes_list(request):
+def Clientes_list(request: HttpRequest) -> HttpResponse:
     query = Cliente.objects.all()
     context = {"objects_list": query}
-    return render(request, "Chicago/Clientes_list.html", context)
+    return render(request, 'Chicago/Clientes_list.html', context)
 
-def Clientes_create(request):
+def Clientes_create(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
         form = ClientesForm()
-    if request.method == "POST":
+        return render(request, "Chicago/Clientes_form.html", {"form": form})
+    elif request.method == "POST":
         form = ClientesForm(request.POST) 
         if form.is_valid():
             form.save()
             return redirect("Chicago:Clientes_list")
         return render(request, "Chicago/Clientes_form.html", {"form": form})
+    return render(request, "Chicago/Clientes_form.html", {"form": ClientesForm()})
 
-def Productos_list(request):
+def Productos_list(request: HttpRequest) -> HttpResponse:
     query = Producto.objects.all()
     context = {"objects_list": query}
     return render(request, "Chicago/Productos_list.html", context)
 
-def Producto_create(request):
+def Producto_create(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
         form = ProductoForm()
     if request.method == "POST":
@@ -40,12 +44,12 @@ def Producto_create(request):
             return redirect("Chicago:Productos_list")
         return render(request, "Chicago/Producto_form.html", {"form": form})
 
-def Ventas_list(request):
+def Ventas_list(request: HttpRequest) -> HttpResponse:
     query = Venta.objects.all()
     context = {"objects_list": query}
     return render(request, "Chicago/Ventas_list.html", context)
 
-def Ventas_create(request):
+def Ventas_create(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
         form = VentasForm()
     if request.method == "POST":
