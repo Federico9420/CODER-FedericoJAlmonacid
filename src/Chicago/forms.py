08bfe84cf8275b143b78bc9a5d032eaf9feca46c
Federico Almonacid <fede.cur.4.agosto@gmail.com>
2025-01-15 23:38:22 -0300

    Registro de usuarios

--- src/Chicago/forms.py
+++ src/Chicago/forms.py
@@ -1,5 +1,6 @@
 from django import forms 
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 from .models import Cliente, Producto, Venta
 
 class ProductoForm(forms.ModelForm):
@@ -20,3 +21,15 @@ class ClientesForm(forms.ModelForm):
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         fields = ["username", "password"]
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2"]
+        help_texts = {'username': ''}
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+        
--- src/Chicago/views.py
+++ src/Chicago/views.py
@@ -1,9 +1,9 @@
-from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.http import HttpResponse, HttpRequest
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm, CustomUserCreationForm
+from django.views.generic import CreateView
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "Chicago/index.html")
@@ -15,5 +15,10 @@ class CustomLoginView(LoginView):
     authentication_form=CustomAuthenticationForm
     template_name= "Chicago/login.html"
     next_page = reverse_lazy("Chicago:index")
+
+class CustomRegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "Chicago/register.html"
+    success_url = reverse_lazy("Chicago:login")
    
     
